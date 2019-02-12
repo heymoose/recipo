1494e66ef8300cd6c3ea6dc905ad5ee1473c2d91
heymoose <marcusanovak@gmail.com>
2019-02-12 10:56:43 -0700

    Some more styling cleanup, add padding and margin to footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import RecipeBuilder from './containers/RecipeBuilder/RecipeBuilder';
-import style from './App.module.scss';
 
 class App extends Component {
     render() {
