77f29a484342f8763327dbbfd32e463dd6a3a4ab
heymoose <marcusanovak@gmail.com>
2019-02-12 10:43:16 -0700

    Add tests for additional Layout elements, clean up styles a bit

--- src/hoc/Layout/Layout.test.js
+++ src/hoc/Layout/Layout.test.js
@@ -6,7 +6,19 @@ describe('Layout', () => {
     let wrapper;
     beforeEach(() => (wrapper = shallow(<Layout />)));
 
+    it('should render a <header />', () => {
+        expect(wrapper.find('header').length).toEqual(1);
+    });
+
     it('should render a <main />', () => {
         expect(wrapper.find('main').length).toEqual(1);
+    });
+
+    it('should render a <aside />', () => {
+        expect(wrapper.find('aside').length).toEqual(1);
+    });
+
+    it('should render a <footer />', () => {
+        expect(wrapper.find('footer').length).toEqual(1);
     });
 });
