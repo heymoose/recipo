7335c2a7b69dc7899678de5a67c9f079ebdac382
heymoose <marcusanovak@gmail.com>
2019-02-11 20:15:01 -0700

    Add sass, mock up a basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import RecipeBuilder from './containers/RecipeBuilder/RecipeBuilder';
-import style from './App.module.css';
+import style from './App.module.scss';
 
 class App extends Component {
     render() {
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import style from './Layout.module.css';
+import style from './Layout.module.scss';
 
 class Layout extends Component {
     render() {
         return (
-            <>
-                <main className={style.Content}>
+            <div className={style.Layout}>
+                <header className={style.Header}>header</header>
+                <aside className={style.Sidebar}>sidebar</aside>
+                <main className={style.Main}>
                     {this.props.children}
                 </main>
-            </>
+                <footer className={style.Footer}>footer</footer>
+            </div>
         );
     }
 }
